4f4286c7ec66ebc3a92a9b23e744b3590c7c4830
afanfian <afanfian14@gmail.com>
2024-02-18 08:59:47 +0700

    feat: add required on form create

--- resources/views/profile/create.blade.php
+++ resources/views/profile/create.blade.php
@@ -7,19 +7,19 @@
         <p class="text-center text-xl font-bold pb-5">Tambahkan Data Karyawan Baru</p>
         <div class="mb-4">
             <label for="name" class="block text-gray-700 font-bold mb-2">Nama Karyawan:</label>
-            <input type="text" name="name" id="name" placeholder="Fian" class="px-4 py-2 border rounded-lg w-full focus:outline-none focus:border-blue-500">
+            <input type="text" name="name" id="name" placeholder="Fian" class="px-4 py-2 border rounded-lg w-full focus:outline-none focus:border-blue-500" required>
         </div>
         <div class="mb-4">
             <label for="date" class="block text-gray-700 font-bold mb-2">Tanggal Lahir:</label>
-            <input type="date" name="date" id="date" value="{{ date('Y-m-d') }}" class="px-4 py-2 border rounded-lg w-full focus:outline-none focus:border-blue-500">
+            <input type="date" name="date" id="date" value="{{ date('Y-m-d') }}" class="px-4 py-2 border rounded-lg w-full focus:outline-none focus:border-blue-500" required>
         </div>
         <div class="mb-4">
             <label for="role" class="block text-gray-700 font-bold mb-2">Jabatan:</label>
-            <input type="text" name="role" id="role" placeholder="Manajer" class="px-4 py-2 border rounded-lg w-full focus:outline-none focus:border-blue-500">
+            <input type="text" name="role" id="role" placeholder="Manajer" class="px-4 py-2 border rounded-lg w-full focus:outline-none focus:border-blue-500" required>
         </div>
         <div class="mb-4">
             <label for="city" class="block text-gray-700 font-bold mb-2">Kota Asal:</label>
-            <input type="text" name="city" id="city" placeholder="Surabaya" class="px-4 py-2 border rounded-lg w-full focus:outline-none focus:border-blue-500">
+            <input type="text" name="city" id="city" placeholder="Surabaya" class="px-4 py-2 border rounded-lg w-full focus:outline-none focus:border-blue-500" required>
         </div>
         <button type="submit" name="submit" class="px-4 py-2 rounded-lg text-white bg-blue-500 hover:bg-blue-600 focus:outline-none">
             Simpan
